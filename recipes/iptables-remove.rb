53997648a1901073c6de241b2620de362c86c622
Philip Hutchins <flipture@gmail.com>
2016-06-15 13:42:47 -0400

    Updating iptables remove to clear all iptables rules

--- recipes/iptables-remove.rb
+++ recipes/iptables-remove.rb
@@ -1,6 +1,14 @@
+# Should only run this once... Don't want to step on other rules toes
+# Maybe just remove /etc/iptables files
 bash 'clear_iptables' do
   code <<-EOH
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -t nat -F
+    iptables -t mangle -F
     iptables -F
+    iptables -X
   EOH
 end
 
